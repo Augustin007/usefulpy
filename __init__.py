109a271a17c6a938405cd83c0d7c94ee0d520fb5
Augustin007 <74737212+Augustin007@users.noreply.github.com>
2020-12-14 13:12:03 -0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,40 @@
+'''
+usefulmodules
+Version: 1.1.1
+Author: Austin Garcia
+
+Simple resources and modules for cleaner programming
+
+LICENSE: See license file.
+
+PLATFORMS:
+These program import python's built in warnings, functools, math, decimal,
+fractions, random, collections and datetime, should work on any python platform
+where these are available.
+
+INSTALLATION:
+Put the folder where Python can see it.
+
+RELEASE NOTES:
+1
+ 1.1
+  Version 1.1.1:
+   This program is started as a collection of smaller programs filled with
+   useful functions...
+  Version 1.1.2:
+   Bugfixes and improvements throughout.
+
+SECTIONS:
+validation v1.1.2
+ This program contains many useful functions for validation of input and output.
+formatting v1.2.2
+ This program contains several useful functions for formatting output.
+mathematics v1.2.3
+ Several mathematical functions plopped together.
+
+'''
+
+try: import validation, formatting, mathematics
+except: from usefulpy import validation, formatting, mathematics
+
+#eof
