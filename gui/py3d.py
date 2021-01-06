c1383ab765bf8ac165976ba476c2824d7b357096
Augustin007 <74737212+Augustin007@users.noreply.github.com>
2021-01-06 11:18:55 -0800

    Update py3d.py

--- gui/py3d.py
+++ gui/py3d.py
@@ -8,6 +8,7 @@ hopefully
 #Math is useful! I almost forgot it
 from usefulpy.mathematics.nmath import *
 degrees()
+
 #Not sure how to make this into a frame... in fact, I didn't think this through
 #I only worked out the math part in my head, checked it on desmos
 # (see https://www.desmos.com/calculator/bnk7wnndk1) and ... opened the .py
@@ -15,13 +16,12 @@ degrees()
 #ooh! Speaking of math... fourier series! I should make something for those too,
 #fourier.py, does that go in gui or math, fourier animations are fun.
 
-
 #The first part is passing a 3d point into a 2d point
 #The way this works (yes... this is an excuse to rant about how great math is
 #the 'give an impassioned rant' button in 3b1b's website is another great excuse)
 # The way this works is that you imagine a 3d vector from a point to an 'eye'
 # (a point with a plane in front of it), where this vector intersects with the
-#plane is the point is should be graphed at.
+#plane is the point is should be graphed at. Math is awesome.
 
 from collections import namedtuple
 Point3d = namedtuple('Point3d', ('x', 'y', 'z'))
@@ -35,15 +35,19 @@ class camera(object):
         self.x = x
         self.y = y
         self.z = z
-        self.location = (x, y, z)
+        
         self.thetax = thetax
         self.thetaz = thetaz
         self.fov = fov
         self._precompute()
     def _precompute(self):
+        # stuff its going to use tons of times so I might as well calculate
+        # now
         thetax = self.thetax
         thetaz = self.thetaz
-        x, y, z = self.location
+        x, y, z = (self.x, self.y, self.z)
+        #print(x, y, z)
+        self.location = (x, y, z)
         self.pre1 = cos(thetaz)
         self.pre2 = sin(thetaz)
         self.pre3 = -cos(thetax)
@@ -127,85 +131,85 @@ class camera(object):
 
     def tiltup(self, degrees):
         self.thetax += degrees
-        self._precompute
+        self._precompute()
 
     def tiltdown(self, degrees):
         self.thetax -= degrees
-        self._precompute
+        self._precompute()
 
     def tiltleft(self, degrees):
         self.thetaz += degrees
-        self._precompute
+        self._precompute()
 
     def tiltright(self, degrees):
         self.thetaz -= degrees
-        self._precompute
+        self._precompute()
 
     def fdx(self, amount):
         self.x += amount
-        self._precompute
+        self._precompute()
 
     def fdy(self, amount):
         self.y += amount
-        self._precompute
+        self._precompute()
 
     def fdz(self, amount):
         self.z += amount
-        self._precompute
+        self._precompute()
 
     def bkx(self, amount):
         self.x -= amount
-        self._precompute
+        self._precompute()
 
     def bky(self, amount):
         self.y -= amount
-        self._precompute
+        self._precompute()
 
     def bkz(self, amount):
         self.z -= amount
-        self._precompute
+        self._precompute()
 
     def fdxy(self, amount): ##Not tested
         xrat = cos(self.thetaz)
         yrat = sin(self.thetaz)
         self.x += amount*xrat
         self.y += amount*yrat
-        self._precompute
+        self._precompute()
 
     def fdyz(self, amount): ##Not tested
         xrat = sin(self.thetax)
         zrat = cos(self.thetax)
         self.y += amount*yrat
         self.z += amount*zrat
-        self._precompute
+        self._precompute()
 
     def fdzx(self, amount): ##Not tested
         xrat = sin(self.thetax)
         zrat = cos(self.thetax)
         self.x += amount*xrat
         self.z += amount*zrat
-        self._precompute
+        self._precompute()
 
     def bkxy(self, amount): ##Not tested
         xrat = cos(self.thetaz)
         yrat = sin(self.thetaz)
         self.x -= amount*xrat
         self.y -= amount*yrat
-        self._precompute
+        self._precompute()
 
     def bkyz(self, amount): ##Not tested
         xrat = sin(self.thetax)
         zrat = cos(self.thetax)
         self.x -= amount*xrat
         self.z -= amount*zrat
-        self._precompute
+        self._precompute()
 
     def bkzx(self, amount): ##Not tested
         xrat = sin(self.thetax)
         zrat = cos(self.thetax)
         self.x -= amount*xrat
         self.z -= amount*zrat
-        self._precompute
+        self._precompute()
 
     def fd(self, amount): ##Not tested
         xrat = cos(self.thetaz)*sin(self.thetax)
@@ -214,7 +218,7 @@ class camera(object):
         self.x += amount*xrat
         self.y += amount*yrat
         self.z += amount*zrat
-        self._precompute
+        self._precompute()
 
     def bk(self, amount): ##Not tested
         xrat = cos(self.thetaz)*sin(self.thetax)
@@ -223,6 +227,14 @@ class camera(object):
         self.x -= amount*xrat
         self.y -= amount*yrat
         self.z -= amount*zrat
-        self._precompute
+        self._precompute()
 
-    
+    def __repr__(self):
+        return f'<(py3d.camera at point{self.location} facing({self.thetax}, {self.thetaz})>'
+
+    #def view(figure) #view a 3d figure #will return polygons squished into 2d
+
+
+#class figure3d (a set of interconnecting 2d figures)
+#class figure2d (a set of interconnecting 1d lines)
+#class line_segment (a pair of points)
