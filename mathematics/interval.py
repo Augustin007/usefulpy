9a3142ce91cab87728fe28fa1d98f46569c21135
Austin Garcia <74737212+Augustin007@users.noreply.github.com>
2021-04-13 12:25:44 -0700

    Update interval.py

--- mathematics/interval.py
+++ mathematics/interval.py
@@ -2,14 +2,28 @@ def _nbin(num, length):
     binnum = bin(num)[2:]
     if len(binnum) >= length: return binnum[-length:]
     return (length - len(binnum))*'0'+binnum
+
+class Interval:
+    def __new__(cls, *args):
+        '''Interval(start, stop, mode) -> interval
+Interval(*intervals) -> unified_interval'''
+        #'''Interval(str) -> interval or unified_interval''' Soon!
+        is_interval = lambda x: isinstance(x, (interval, unified_interval))
+        if any(map(is_interval, args)):
+            if all(map(is_interval, args)):
+                return unified_interval(*args)
+            raise TypeError('All arguments must be intervals')
+        if len(args) in (2, 3):
+            return interval(*args)
+        raise TypeError('Interval error')
+
 class interval:
     start:float
     stop:float
     _check:type(lambda:None)
     _repstr:str
     mode: int
-    finished: False
-    def __new__(cls, start:float, stop:float, mode = 'open'):
+    def __init__(self, start:float, stop:float, mode = 'open'):
         if type(mode) is str and mode not in ('open', 'left', 'right', 'close'):
             raise ValueError('Invalid Mode')
         elif type(mode) is int and mode not in range(0, 4):
@@ -17,30 +31,29 @@ class interval:
         elif type(mode) in (int, str): pass
         else:
             raise TypeError('Invalid Mode')
-        self = super(interval, cls).__new__(cls)
         self.start = float(start)
         self.stop = float(stop)
         if mode in ('open', 0):
             self._check = (lambda x: (x>start) and (x<stop))
             self._repstr = '('+str(self.start)+', '+str(self.stop)+')'
             self.mode = 0
-            return self
+            return
         if mode in ('right', 1):
             self._check = (lambda x: (x>start) and (x<=stop))
             self._repstr = '('+str(self.start)+', '+str(self.stop)+']'
             self.mode = 1
-            return self
+            return
         if mode in ('left',2):
             self._check = (lambda x: (x>=start) and (x<stop))
             self._repstr = '['+str(self.start)+', '+str(self.stop)+')'
             self.mode = 2
-            return self
+            return
         if mode in ('close', 3):
             self._check = (lambda x: (x>=start) and (x<=stop))
             self._repstr = '['+str(self.start)+', '+str(self.stop)+']'
             self.mode = 3
-            return self
-        raise Exception('Internal Error Occured: This code shouldn\'t be possible to access')
+            return
+        raise Exception('Internal Error Occured: This code shouldn\'t be possible to access, as the above statements account for every possibility.')
 
     def __contains__(self, x):
         return self._check(x)
@@ -69,14 +82,16 @@ class interval:
                 return interval(self.start, other.stop, int(_nbin(self.mode, 2)[0]+_nbin(other.mode, 2)[-1], base = 2))
             return unified_interval(self, other)
         if type(other) is unified_interval:
-            pass
-            
+            return unified_interval(*other.intervals, self)
         return NotImplemented
 
     def __ror__(self, other):
         return NotImplemented
 
     def __xor__(self, other):
+        if type(other) is interval:
+            if other.start in self:
+                if other.stop in self: return other
         return NotImplemented
 
     def __rxor__(self, other):
@@ -100,10 +115,8 @@ class interval:
 
 class unified_interval:
     intervals: tuple[interval]
-    def __new__(cls, *intervals):
-        self = super(unified_interval, cls).__new__(cls)
+    def __init__(self, *intervals):
         self.intervals = tuple(sorted(intervals))
-        return self
 
     def __contains__(self, x):
         for inter in self.intervals:
