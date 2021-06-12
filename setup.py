40048fd240d6a3210632dfab7c2d41654256abdd
Austin Garcia <74737212+Augustin007@users.noreply.github.com>
2021-06-12 12:00:36 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="usefulpython",
-    version="0.1.4",
+    version="0.1.5",
     author="Augustin Garcia",
     author_email="albusdumbledore101123@gmail.com",
     description="tools for a cleaner python experience",
