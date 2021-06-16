9c8eab52a1343383d3c82aedff98b094971af0ff
Austin Garcia <74737212+Augustin007@users.noreply.github.com>
2021-06-16 15:03:53 -0700

    Preparing for new lighting system

--- src/usefulpy/gui/py3d/light_square_test.py
+++ src/usefulpy/gui/py3d/light_square_test.py
@@ -1,17 +1,21 @@
-from usefulpy import mathematics as math
+'''
+preparing for new lighting system
+'''
+from .. import mathematics as math
 
 def light(L, p, n, t):
     h_raw = L-p
     h = abs(h_raw)
     d = abs((h_raw/h)-n)
-    if 1:
+    if d > math.sqrt(2)/2:
+        return 0
+    if testing:
         print(d, h)
     return light_square(h, d, t)
 
 def light_square(h, d, t):
     square = 4*(t**2)
     d_cos, d_sin = cos_and_sin(d)
-    d_squared = d**2
     offset_x = t*d_sin
     offset_z = t*d_cos
     t_off = math.hypot(t, offset_z)
@@ -21,12 +25,12 @@ def light_square(h, d, t):
     s2 = math.quaternion(0, (h-offset_x)/A_b, -offset_z/A_b, t/A_b) # top right
     s3 = math.quaternion(0, (h-offset_x)/A_b, -offset_z/A_b, -t/A_b) # bottom right
     s4 = math.quaternion(0, (h+offset_x)/A_t, offset_z/A_t, -t/A_t) #bottom left
-    if 1:
+    if testing:
         print()
         print(f'1: {s1}',f'2: {s2}', f'3: {s3}', f'4: {s4}', sep = '\n')
         print(abs(s1), abs(s2), abs(s3), abs(s4))
 
-    if 0:
+    if testing:
         print()
         print(abs(s1-s4), abs(s2-s3), abs(s1-s3))
 
@@ -37,7 +41,7 @@ def light_square(h, d, t):
     β_cos, β_sin = cos_and_sin(abs(s3-s4))
     γ_cos, γ_sin = cos_and_sin(abs(s2-s3))
 
-    if 1:
+    if testing:
         print()
         print('δ:', δ_cos, δ_sin)
         print('α:', α_cos, α_sin)
@@ -52,13 +56,13 @@ def light_square(h, d, t):
     A_cos = A_cos_numer/A_cos_denom
     B_cos = B_cos_numer/B_cos_denom
 
-    if 1:
+    if testing:
         print()
         print(A_cos, B_cos)
     
     A = math.acos(A_cos)
     B = math.acos(B_cos)
-    if 1:
+    if testing:
         print()
         print(f'A and B: {A}, {B}')
     # with approximately 1.7374629934615633, (this is made from an imprecise
@@ -66,7 +70,7 @@ def light_square(h, d, t):
     # light(math.quaternion(0), math.quaternion(0, 2), math.quaternion(0, -1), 1)
     s_poly = 2*A + 2*B - math.tau
     #calculated value
-    if 1:
+    if testing:
         print()
         print('Function:', s_poly)
         #imprecise approximation
@@ -89,6 +93,7 @@ def test(n=0):
     return light(math.quaternion(0), math.quaternion(0, 2), math.quaternion(0, -1, n).normal(), 1)
 
 if __name__ == '__main__':
+    testing =True
     print('test1:')
     print(test())
     print()
