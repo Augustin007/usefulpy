78e8c0d6f75740044f15df098e4e6f5bfd023901
Austin Garcia <74737212+Augustin007@users.noreply.github.com>
2021-09-12 13:39:20 -0700

    Configuration Changes

--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="usefulpython",
-    version="0.2.0",
-    author="Augustin Garcia",
-    author_email="albusdumbledore101123@gmail.com",
-    description="tools for a cleaner python experience",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Augustin007/usefulpy",
-    project_urls={
-        "Bug Tracker": "https://github.com/Augustin007/usefulpy",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.9",
-)
+if __name__ == '__main__':
+    setuptools.setup()
