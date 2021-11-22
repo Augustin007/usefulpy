73d4fb322160887efd412d7a0489a623a71b8ff6
Austin Garcia <74737212+Augustin007@users.noreply.github.com>
2021-11-22 09:10:22 -0800

    Update test_usefulpy.py
    Tests for basenum started

---
+++ tests/test_usefulpy.py
@@ -0,0 +1,22 @@
+from usefulpy import *
+import pytest
+
+
+def test_basenum_init():
+    ...
+
+def test_basenumlessthanten():
+    test = mathematics.basenum('---1010', 2)
+    assert test.base = 2
+    assert test.num = '1010'
+    assert test.floatpart = ''
+    assert test.pos = False
+
+def test_basenumlessthanten():
+    test = mathematics.basenum('1010.1', 3)
+    assert test.base = 3
+    assert test.num = '1010'
+    assert test.floatpart = '1'
+    assert test.pos = True
+
+
