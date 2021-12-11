34a1cefc171a87074baa9f7b8c8cb589e5055f3a
Austin Garcia <74737212+Augustin007@users.noreply.github.com>
2021-12-10 20:46:15 -0800

    Update vector.py

--- src/usefulpy/mathematics/vector.py
+++ src/usefulpy/mathematics/vector.py
@@ -13,9 +13,11 @@ RELEASE NOTES:
  0.0
   Version 0.0.0:
    A simple linear algebra calculator
+ 0.1
+  Version 0.1.0:
+   
 '''
 
-### DUNDERS ###
 __author__ = 'Augustin Garcia'
 __version__ = '0.0.0'
 
@@ -23,28 +25,34 @@ from .. import validation as _validation
 from .. import formatting as _formatting
 from . import nmath as _nmath
 import functools
+import math
 
 class vector:
-    repr_mode = 'simple'
+    repr_mode:int = 0
+    scalars:tuple[float]
+    dims:int
+    magnitude:float
     def __new__(cls, *scalars, dims = None):
-        if dims is None:
-            dims = len(scalars)
-        for n in scalars:
-            if not _validation.is_float(n):
-                print(scalars)
-                raise TypeError('All basis-vector scalars must be valid numbers')
-        scalars = tuple(float(n) for n in scalars)
         v = super(vector, cls).__new__(cls)
+        scalars = tuple(map(float, scalars))
+        if dims is None: 
+            dims = len(scalars)
+        else:
+            dims = int(dims)
+            if dims < len(scalars):
+                raise ValueError('Too many scalars for number of dimensions')
+            if dims > len(scalars):
+                scalars = tuple(list(scalars)+[0]*(dims-len(scalars)))
         v.scalars = scalars
         v.dims = dims
-        v.magnitude = _nmath.hypot(*scalars)
+        v.magnitude = math.hypot(*scalars)
         return v
     
     def __add__(v1, v2):
         if not isinstance(v2, vector):
             if _validation.is_float(v2):
-                return vector(*([v1.scalars[0]+v2]+list(v1.scalars[1:])))
-            raise TypeError(f'cannot add a vector and a {type(v2).__name__}')
+                return vector(*([v1.scalars[0]+float(v2)]+list(v1.scalars[1:])))
+            return NotImplemented
         if v1.dims == v2.dims:
             return vector(*[a+b for a, b in zip(v1.scalars, v2.scalars)])
         if v1.dims < v2.dims:
@@ -54,12 +62,14 @@ class vector:
         end = list(v1.scalars[v2.dims:])
         start = [a+b for a, b in zip(v1.scalars, v2.scalars)]
         return vector(*(start+end))
+    
+    __radd__ = __add__
 
-    def _mstr(v):
+    def __mulstr__(v):
         return _formatting.multline(*v.scalars)
-
+    
     def __repr__(v):
-        if v.repr_mode == 'simple':
+        if v.repr_mode == 0:
             return 'vector'+str(v.scalars)
         mstr = _formatting.multline(*v.scalars)
         h = mstr.getheight()
@@ -70,11 +80,6 @@ class vector:
         bottom = '‾' + w*' '+'‾'
         return '\n'.join((top, mstr, bottom))
         
-        
-    __str__ = __repr__
-
-    __radd__ = __add__
-
     def __sub__(v1, v2):
         if not isinstance(v2, vector):
             if _validation.is_float(v2):
@@ -93,7 +98,9 @@ class vector:
     def __rsub__(v1, v2):
         if _validation.is_float(v2):
             return vector(*([v2-v1.scalars[0]]+[-n for n in v1.scalars[1:]]))
-        raise TypeError(f'cannot subtract a vector from a {type(v2).__name__}')
+        return NotImplemented
+    
+    __str__ = __repr__
 
     def __pos__(v):
         return v
@@ -102,43 +109,53 @@ class vector:
         return vector(*[-n for n in v.scalars])
     
     def __mul__(v, s):
-        if _validation.is_float(s):
-            return vector(*[n*s for n in v.scalars])
-        raise TypeError(f'Vectors can only be multiplied by floats, not {type(s).__name__}')
-
+        s = float(s)
+        try: return vector(*[n*s for n in v.scalars])
+        except: return NotImplemented
     __rmul__ = __mul__
-
     def __div__(v, s):
-        if _validation.is_float(s):
-            return vector(*[n/s for n in v.scalars])
-        raise TypeError(f'Vectors can only be divided by floats, not {type(s).__name__}')
+        s = float(s)
+        try: return vector(*[n/s for n in v.scalars])
+        except: return NotImplemented
+    
+    def __rdiv__(v, s): return NotImplemented
 
+    def __pow__(v, m): return NotImplemented
+
+    def __rpow__(v, m): return NotImplemented
+    
     def __iter__(v):
         return v.scalars.__iter__()
+    
+    def dot(v1, v2):
+        if v1.dims < v2.dims:
+            v1 = vector(*v1.scalars, v2.dims)
+        if v2.dims < v1.dims:
+            v2 = vector(*v2.scalars, v1.dims)
+        return sum(map(lambda a, b: a*b, zip(v1, v2)))
+    
+    def __getitem__(v, i): 
+        return v.scalars[i]
+
 
 class matrix:
-    repr_mode = 'simple'
+    repr_mode = 0
     def __new__(cls, *vectors):
-        nvectors = []
-        for n in vectors:
-            if isinstance(n, vector): nvectors.append(n); continue
-            try: nvectors.append(vector(*n)); continue
-            except: pass
-            try: nvectors.append(vector(n)); continue
-            except: pass
-            raise TypeError('Innappropriate argument for matrix')
-        a = max([v.dims for v in nvectors])
-        b = len(nvectors)
-        vectors = []
-        for v in nvectors:
-            if v.dims != a:
-                vectors.append(vector(*(list(v.scalars) + list((a-v.dims)*'0'))))
-                continue
-            vectors.append(v)
+        if len(vectors) == 0:
+            m = super(matrix, cls).__new__(cls)
+            m.out_dim = 0
+            m.in_dim = 0
+            m.vectors = m._vectors = ()
+        vectors = tuple([v if type(v) is vector else vector(*v) for v in vectors])
+        a = max([v.dims for v in vectors])
+        b = len(vectors)
+        vectors1 = tuple([vector(*v.scalars, dims = a) for v in vectors])
+        vectors2 = tuple([vector(*v.scalars, dims = max(a, b)) for v in vectors])
         m = super(matrix, cls).__new__(cls)
         m.out_dim = a
         m.in_dim = b
-        m.vectors = tuple(vectors)
+        m.vectors = tuple(vectors1)
+        m._vectors = tuple(vectors2)
         return m
     
     def _mul_vector(m, v):
@@ -148,7 +165,7 @@ class matrix:
                 nvectors.append(s*v1)
             return sum(nvectors)
         elif m.in_dim > v.dims:
-            nv = vector(*(list(v.scalars) + list((m.in_dim-v.dims)*'0')))
+            nv = vector(*(list(v.scalars)), dims = m.in_dim)
             return m*nv
         
 
@@ -165,18 +182,18 @@ class matrix:
         if isinstance(m1, matrix):
             return m._mul_matrix(m1)
         return NotImplemented
-
-    def _mstr(m):
-        mstrs = tuple([v._mstr() for v in m.vectors])
+    
+    def __mulstr__(m):
+        mstrs = tuple([v.__mulstr__() for v in m.vectors])
         return functools.reduce(lambda a, b: a+ ' '+b, mstrs)
 
     def __repr__(m):
-        if m.repr_mode == 'simple':
+        if m.repr_mode == 0:
             return 'matrix' + str(m.vectors)
-        mstr = m._mstr()
+        mstr = m.__mulstr__()
         h = mstr.getheight()
         w = mstr.getwidth()
-        side = _formatting.multline(*tuple('|'*h))
+        side = _formatting.multline(*('|'*h))
         mstr = str(side + mstr + side)
         top = '_'+w*' '+'_'
         bottom = '‾' + w*' '+'‾'
@@ -184,12 +201,46 @@ class matrix:
 
     def __iter__(m):
         return m.vectors.__iter__()
+    
+    def __float__(m):
+        if len(m.vectors) == 1:
+            if len(m.vectors[0].scalars) == 1:
+                return m.vectors[0].scalars[0]
+        return NotImplemented
+    
+    def get_column(m, i, zeros = False):
+        if zeros: l = m._vectors[i]
+        else: l = m.vectors[i]
+        try: return matrix(*l)
+        except: return matrix(l)
+    
+    def get_row(m, i, zeros = False):
+        if zeros: data = [v[i] for v in m._vectors]
+        else: data = [v[i] for v in m.vectors]
+        try: return matrix(*data)
+        except: return matrix(*map(vector, data))
 
-def _organize(*row_info):
-    boxln = max(map(len, map(str, _validation.flatten(row_info))))
-    a = [(' '.join(map(lambda n: str(n).ljust(boxln), row))) for row in row_info]
-    return tuple(a)
+    def __getitem__(m, i):
+        if type(i) is tuple:
+            if len(i) == 2: 
+                return m.get_row(i[1]).get_column(i[0])
+            if len(i) == 3:
+                return m.get_row(i[1], i[2]).get_column(i[0], i[2])
+            raise IndexError("Invalid Index")
+        if type(i) in (slice, int): 
+            return m.get_column(i)
 
-if __name__ == '__main__': #For testing reasons
-    vector.repr_mode = ''
-    matrix.repr_mode = ''
+
+    def det(m):
+        if m.in_dim > m.out_dim:
+            return 0
+        if m.in_dim == 2: 
+            return m[0,True]*m[1, 1,True]-m[0, 1,True]*m[1, 0,True], 
+        sum = 0
+        for n in range(m.in_dim):
+            num = float(m[n, 0])
+            m_sub = matrix(*m[:n, 1:, True], *m[(n+1):, 1:, True])
+            print(m_sub)
+            if n&1: sum += num*m_sub.det()
+            else: sum -=  num*m_sub.det()
+        
