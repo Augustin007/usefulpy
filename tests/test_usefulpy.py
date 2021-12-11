d69191a9d01e871dbf5ab623df2dc137a9012422
Austin Garcia <74737212+Augustin007@users.noreply.github.com>
2021-12-10 17:11:15 -0800

    Bugfix in tests

--- tests/test_usefulpy.py
+++ tests/test_usefulpy.py
@@ -10,13 +10,13 @@ def test_basenumlessthanten():
     assert test.base == 2
     assert test.num == '1010'
     assert test.floatpart == ''
-    assert test.pos == False
+    assert test.Negative == True
 
 def test_basenumlessthanten1():
     test = mathematics.basenum('1010.1', 3)
     assert test.base == 3
     assert test.num == '1010'
     assert test.floatpart == '1'
-    assert test.pos == True
+    assert test.Negative == False
 
 
