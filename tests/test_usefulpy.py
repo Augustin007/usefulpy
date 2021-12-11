b04235a6c1137eaeb8237a6adc9af0c82f3d4eca
Austin Garcia <74737212+Augustin007@users.noreply.github.com>
2021-12-10 16:58:03 -0800

    Syntax Error Fix

--- tests/test_usefulpy.py
+++ tests/test_usefulpy.py
@@ -7,16 +7,16 @@ def test_basenum_init():
 
 def test_basenumlessthanten():
     test = mathematics.basenum('---1010', 2)
-    assert test.base = 2
-    assert test.num = '1010'
-    assert test.floatpart = ''
-    assert test.pos = False
+    assert test.base == 2
+    assert test.num == '1010'
+    assert test.floatpart == ''
+    assert test.pos == False
 
 def test_basenumlessthanten():
     test = mathematics.basenum('1010.1', 3)
-    assert test.base = 3
-    assert test.num = '1010'
-    assert test.floatpart = '1'
-    assert test.pos = True
+    assert test.base == 3
+    assert test.num == '1010'
+    assert test.floatpart == '1'
+    assert test.pos == True
 
 
