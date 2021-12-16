01ee99c112ac314eca1c44075d336268cedd7d89
Austin Garcia <74737212+Augustin007@users.noreply.github.com>
2021-12-16 09:22:39 -0800

    Increasing coverage

--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -1,1 +1,8 @@
 import pytest
+from usefulpy import formatting
+
+
+def test_compose():
+    assert formatting.ComposeNumber(10) == 'ten'
+    assert formatting.ComposeNumber(100) == 'one hundred'
+    assert formatting.ComposeNumber(12683524647636) == 'twelve trillion six hundred eighty three billion five hundred twenty four million six hundred forty seven thousand six hundred thirty six'
--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,1 +1,19 @@
 import pytest
+from usefulpy import validation
+
+
+def test_checks():
+    assert validation.is_integer('1')
+    assert validation.is_integer('1.0')
+    assert validation.is_integer(1)
+    assert validation.is_integer(1.0)
+    assert not validation.is_integer(1.1)
+    assert not validation.is_integer('hello')
+    assert not validation.is_integer('1.2')
+    assert validation.is_float('1')
+    assert validation.is_float('1.0')
+    assert validation.is_float(1)
+    assert validation.is_float(1.0)
+    assert validation.is_float(1.1)
+    assert not validation.is_float('hello')
+    assert validation.is_float('1.2')
