9534a2762765ad6df218f95523a1d830a4c7fcbb
Austin Garcia <74737212+Augustin007@users.noreply.github.com>
2021-12-17 13:56:51 -0800

    Increasing coverage

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,8 +1,34 @@
 import pytest
 from usefulpy import validation
+('YesOrNo', 'are_floats', 'are_integers', 'bool_', 'boolinput',
+'floatinput', 'floatlistinput', 'fromdatainput', 'getYesOrNo', 'intinput',
+'intlistinput', 'is_complex', 'is_float', 'is_floatlist', 'is_integer',
+'is_intlist', 'isbool', 'makelist', 'merge_dicts', 'multi_in', 'multicheck',
+'trycomplex', 'tryfloat', 'tryint', 'trynumber', 'trytype', 'validdate',
+'validinput', 'validquery')
 
+def test_YesOrNo_true():
+    assert validation.YesOrNo('y')
+    assert validation.YesOrNo('yes')
+    assert validation.YesOrNo('yeah')
+    assert validation.YesOrNo('of course')
+    assert validation.YesOrNo('Why not')
+    assert validation.YesOrNo('sure')
 
-def test_checks():
+def test_YesOrNo_false():
+    assert not validation.YesOrNo('n')
+    assert not validation.YesOrNo('no')
+    assert not validation.YesOrNo('nope')
+
+def test_are_floats():
+    assert validation.are_floats(1, '1.0', 1.1, 1.0)
+    assert not validation.are_floats(1, '1.0', 1.1, 1.0, 'hello')
+
+def test_are_integers():
+    assert validation.are_integers(1, '1.0', 1.0)
+    assert not validation.are_integers(1.1, 1, 2)
+
+def test_is_integer():
     assert validation.is_integer('1')
     assert validation.is_integer('1.0')
     assert validation.is_integer(1)
@@ -10,6 +36,8 @@ def test_checks():
     assert not validation.is_integer(1.1)
     assert not validation.is_integer('hello')
     assert not validation.is_integer('1.2')
+
+def test_is_float():
     assert validation.is_float('1')
     assert validation.is_float('1.0')
     assert validation.is_float(1)
